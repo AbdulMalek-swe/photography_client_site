015c0030f76c860684e351db46ddb85d24f27481
AbdulMalek-swe <AbdulMalek-swe>
2023-03-07 23:36:40 +0600

    start project photography

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React  from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import './App.css';
+import {increment} from './redux/features/counterSlice';
 
 function App() {
+  const state = useSelector(state =>state)
+  console.log(state);
+  console.log(state);
+const dispatch = useDispatch()
+const handleIncrement = () =>{
+dispatch( increment())
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={handleIncrement}>submit my code</button>
     </div>
   );
 }
